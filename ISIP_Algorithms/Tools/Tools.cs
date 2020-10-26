62b402bf4a0fe71c4e245234052da90b9a5f7746
Cxlaur <58987501+Cxlaur@users.noreply.github.com>
2020-10-26 10:00:07 +0200

    Add project files.

---
+++ ISIP_Algorithms/Tools/Tools.cs
@@ -0,0 +1,54 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Windows;
+namespace ISIP_Algorithms.Tools
+{
+    public class Tools
+    {
+        public static Image<Gray, byte> Invert(Image<Gray, byte> InputImage)
+        {
+            Image<Gray, byte> Result = new Image<Gray, byte>(InputImage.Size);
+            for (int y = 0; y < InputImage.Height; y++)
+            {
+                for (int x = 0; x < InputImage.Width; x++)
+                {
+                    Result.Data[y, x, 0] = (byte)(255 - InputImage.Data[y, x, 0]);
+                }
+            }
+            return Result;
+        }
+        public static Image<Gray, byte> Mirror(Image<Gray, byte> InputImage)
+        {
+            Image<Gray, byte> Result = new Image<Gray, byte>(InputImage.Size);
+            for (int y = 0; y < InputImage.Height; y++)
+            {
+                for (int x = 0; x < InputImage.Width; x++)
+                {
+                    Result.Data[y, InputImage.Width - x-1, 0] = (byte)( InputImage.Data[y, x, 0]);
+                }
+            }
+            return Result;
+        }
+        public static Image<Gray, byte> Contrast(Image<Gray, byte> InputImage,double m, float E)
+        {
+            double[] LUT = new double[256];
+            double c= Math.Pow(m,E)/(255*(Math.Pow(255,E)+Math.Pow(m,E)));
+
+            for (int index=0;index<256;index++)
+            {
+                LUT[index] =255*((Math.Pow(index,E)/(Math.Pow(index,E)+Math.Pow(m,E)))+(c*index));
+            }
+
+            Image<Gray, byte> Result = new Image<Gray, byte>(InputImage.Size);
+            for (int y = 0; y < InputImage.Height; y++)
+            {
+                for (int x = 0; x < InputImage.Width; x++)
+                {
+                    Result.Data[y, x, 0] = (byte)LUT[( InputImage.Data[y, x, 0])];
+                }
+            }
+            return Result;
+        }
+    }
+}
---
+++ ISIP_FrameworkGUI/Windows/EmDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ISIP_FrameworkGUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for EmDialog.xaml
+    /// </summary>
+    public partial class EmDialog : Window
+    {
+
+        MainWindow Main;
+        public EmDialog(MainWindow w)
+        {
+            Main = w;
+            InitializeComponent();
+        }
+
+        private void Confirm_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(E_value.Text.Length!=0 && m_value.Text.Length!=0)
+            {
+                Main.setmValue(double.Parse(m_value.Text));
+                Main.setEValue(float.Parse(E_value.Text));
+                Main.ContrastActivation();
+                Close();
+            }
+        }
+
+    }
+}
